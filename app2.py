eb7b1209e272787864c50d2b864296ec3ac01263
jycheon <chun3720@hotmail.com>
2022-12-21 23:36:36 +0900

    deadent

--- app2.py
+++ app2.py
@@ -1,4 +1,5 @@
 import streamlit as st
+
 # import numpy as np
 # import pandas as pd
 # import os
@@ -10,22 +11,17 @@ import pathlib
 import src.utils
 import os
 
-# import matplotlib.pyplot as 
+# import matplotlib.pyplot as
 
 # dummy = "0928 18-61-1 LiP-ECDEC_02_CstC.txt"
 # dummy2 = "0928 18-61-1 LiP-ECDEC_02_CstC_cycle_4.csv"
 curr_path = pathlib.Path().resolve()
 example_path = os.path.join(curr_path, "example")
-choice = st.selectbox("Select to analyze",
-                      ("Supercap", "Battery")
-                      )
+choice = st.selectbox("Select to analyze", ("Supercap", "Battery"))
 st.write("You selected: ", choice)
 
-    
-    
-    
-    
-    # 
+
+#
 # import pandas as pd
 if choice == "Supercap":
     st.write("available raw data: .mpt file only ")
@@ -33,17 +29,30 @@ if choice == "Supercap":
     agree = st.checkbox("See example analysis")
     if agree:
         file = os.path.join(example_path, "supycap.mpt")
-        st.button("Supercap analysis", on_click=src.utils.supycap, kwargs = {"file" : file, "example": True})
+        st.button(
+            "Supercap analysis",
+            on_click=src.utils.supycap,
+            kwargs={"file": file, "example": True},
+        )
     else:
         file = st.file_uploader(label="select file ")
 
-        st.button("Supercap analysis", on_click=src.utils.supycap, kwargs = {"file" : file})
+        st.button(
+            "Supercap analysis", on_click=src.utils.supycap, kwargs={"file": file}
+        )
 
 elif choice == "Battery":
-    files = st.file_uploader(label = "select files (shoud include both DC and CYC file) :", accept_multiple_files = True)
-    
-    st.button("Battery analysis", on_click = src.utils.battery_wonatech, kwargs = {"files": files})
-    
+    files = st.file_uploader(
+        label="select files (shoud include both DC and CYC file) :",
+        accept_multiple_files=True,
+    )
 
-    
-    
+    st.button(
+        "Battery analysis", on_click=src.utils.battery_cycle, kwargs={"files": files}
+    )
+
+    st.button("Battery curve", on_click=src.utils.battery_curve)
+    # cycle_list = list(map(str, range(len(curve_object))))
+
+    # cycle_choice = st.multiselect("Selet cycle number to display", cycle_list)
+    # st.write("You selected: ", cycle_choice)
--- src/utils.py
+++ src/utils.py
@@ -6,91 +6,101 @@ from src.supycap import Supycap_obj, Supycap_example
 from src.Battery_wonatech import LIB_cyc, LIB_tot, LIB_sep
 import plotly.express as px
 import plotly.graph_objects as go
+import pandas as pd
 
-def supycap(file, example = False):
-    
+
+def supycap(file, example=False):
+
     # supycap1 = Supycap_obj(file)
-    
+
     if example:
         sc1 = Supycap_example(file)
     else:
         sc1 = Supycap_obj(file)
-    
-    st.line_chart(sc1.df, x = "Time", y = "Volt")
-    
+
+    st.line_chart(sc1.df, x="Time", y="Volt")
+
     curr = sc1.df.loc[0]["Curr"]
-    
+
     read_file = sc1.exported_file
-    
-    supercap = Load_capacitor(read_file, ESR_method=2, current = curr, cap_grav =  False, cap_method =1)
+
+    supercap = Load_capacitor(
+        read_file, ESR_method=2, current=curr, cap_grav=False, cap_method=1
+    )
     fig, ax = plt.subplots()
-    fig = supercap.Check_analysis(begin =1, end = len(sc1.cycles)-1)
-    st.set_option('deprecation.showPyplotGlobalUse', False)
+    fig = supercap.Check_analysis(begin=1, end=len(sc1.cycles) - 1)
+    st.set_option("deprecation.showPyplotGlobalUse", False)
     st.pyplot(fig)
-    
-    supercap2 = Load_capacitor(read_file, ESR_method=2, current = curr, cap_grav =  False, cap_method =2)
+
+    supercap2 = Load_capacitor(
+        read_file, ESR_method=2, current=curr, cap_grav=False, cap_method=2
+    )
     fig3, ax3 = plt.subplots()
-    fig3 = supercap2.Check_analysis(begin =1, end = len(sc1.cycles)-1)
+    fig3 = supercap2.Check_analysis(begin=1, end=len(sc1.cycles) - 1)
     st.pyplot(fig3)
-    
+
     fig2, ax2 = plt.subplots()
     cap_results = np.array(supercap.cap_ls)
     cap_results2 = np.array(supercap2.cap_ls)
-    ax2.scatter(range(cap_results.shape[0]), cap_results*1000, s = 20**2, marker = 'o', c = "r", alpha=0.5, label = "method1: lower half")
-    ax2.scatter(range(cap_results2.shape[0]), cap_results2*1000, s = 20**2, marker = 'o', c = "b", alpha=0.5, label = "method2: upper half")
-    plt.xlabel("number of cycles", fontsize = 20)
-    plt.xticks(fontsize = 15)
-    plt.ylabel("Capacitance (mF)", fontsize = 20)
-    plt.yticks(fontsize = 15)
-    plt.legend(loc = "center", fontsize = 20)
-    
+    ax2.scatter(
+        range(cap_results.shape[0]),
+        cap_results * 1000,
+        s=20**2,
+        marker="o",
+        c="r",
+        alpha=0.5,
+        label="method1: lower half",
+    )
+    ax2.scatter(
+        range(cap_results2.shape[0]),
+        cap_results2 * 1000,
+        s=20**2,
+        marker="o",
+        c="b",
+        alpha=0.5,
+        label="method2: upper half",
+    )
+    plt.xlabel("number of cycles", fontsize=20)
+    plt.xticks(fontsize=15)
+    plt.ylabel("Capacitance (mF)", fontsize=20)
+    plt.yticks(fontsize=15)
+    plt.legend(loc="center", fontsize=20)
+
     st.pyplot(fig2)
-    
-    
-def battery_wonatech(files):
-    
+
+
+def battery_cycle(files):
+
     DC = [_ for _ in files if _.name.endswith("DC.csv")][0]
     CYC = [_ for _ in files if _.name.endswith("CYC.csv")][0]
-    
+
     profile_obj = LIB_tot(DC)
     cycle_obj = LIB_cyc(CYC)
-    
-    
+
     mass = cycle_obj.raw.mass.iloc[0]
-    df, curve_object = profile_obj.get_separation(mass)
-    
+    df = profile_obj.get_separation(mass)
+    profile_obj.raw.to_parquet("raw_with_mass.pqt")
+
     # print1
     # fig, ax = plt.subplots()
     # ax.scatter(df["Cycle"], df["Specific Capacity (Ah/g)"], s = 10**2, alpha = 0.5)
     # plt.xlabel("Cycles", fontsize = 'large')
-    # plt.ylabel("Specific Capacity (Ah/g)", fontsize = 'large') 
+    # plt.ylabel("Specific Capacity (Ah/g)", fontsize = 'large')
     # st.pyplot(fig)
 
-    fig2 = px.scatter(df,
-                      x = "Cycle",
-                      y = "Specific Capacity (Ah/g)" 
-                      )
+    fig2 = px.scatter(df, x="Cycle", y="Specific Capacity (Ah/g)")
     # Use the Streamlit theme.
     # This is the default. So you can also omit the theme argument.
-    fig2.update_traces(marker_size = 10)
+    fig2.update_traces(marker_size=10)
     st.plotly_chart(fig2, theme="streamlit", use_container_width=True)
-    
-    cycle_list = list(map(str, range(len(curve_object))))
-    
-    cycle_choice = st.multiselect("Selet cycle number to display", cycle_list)
-    st.write("You selected: ", cycle_choice)
-    
-    
-    
-    
-    
+
     # single_curve = curve_object[0]
-    
+
     # single_obj = LIB_sep(single_curve)
     # single_obj.get_GCD()
-    
+
     # fig3 = go.Figure()
-    
+
     # fig3.add_trace(go.Scatter(x = single_obj.df["q_c"]
     #                        ,y = single_obj.df["v_c"]
     #                        ,mode = 'lines'
@@ -107,10 +117,11 @@ def battery_wonatech(files):
     #                    ,xaxis_title = dict(text = "Capacity (Ah/g)")
     #                    ,yaxis_title = dict(text = "Voltage (V)")
     #                    )
-    
 
     # st.plotly_chart(fig3)
-    
-    
-    
-    
+
+
+def battery_curve():
+    df = pd.read_parquet("raw_with_mass.pqt")
+
+    st.write(df.columns)
